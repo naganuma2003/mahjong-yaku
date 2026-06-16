a99e828313e3beaf57fc92a1be90111f8c4967cd
naganuma2647 <naganuma2647@gmail.com>
2026-06-16 14:51:01 +0900

    feat: 選手詳細に成績テキストコピーボタン📄追加

--- pro/app.js
+++ pro/app.js
@@ -681,6 +681,7 @@ function renderDetail(p) {
     (totalPlayoffs > 0 ? '<span class="playoff-badge" title="決定戦進出' + totalPlayoffs + '回">★決定戦×' + totalPlayoffs + '</span>' : '') +
     '<button class="fav-toggle-btn' + (isFav ? " active" : "") + '" id="favToggleBtn" title="お気に入り">' + (isFav ? "★" : "☆") + '</button>' +
     '<button class="copy-link-btn" onclick="copyPlayerLink()" title="URLをコピー">🔗</button>' +
+    '<button class="copy-link-btn" id="copyStatsBtn" onclick="copyPlayerStats()" title="成績をテキストコピー">📄</button>' +
     '<a class="x-share-btn" href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(p.name + ' - 麻雀プロ検索') + '&url=' + encodeURIComponent(location.origin + location.pathname + '?p=' + encodeURIComponent(p.id)) + '" target="_blank" rel="noopener" title="Xでシェア">𝕏</a>';
   if (isMultiOrg) {
     html += '<span class="transfer-badge">移籍歴あり</span>';
